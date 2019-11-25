b991c186ad1662962bd1fa2250ea26d94452d5b3
gamer05 <gamer05@list.ru>
2019-11-25 14:07:44 +0300

    CRA + Redux.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = (props) => {
+  const {
+    add,
+    addNum,
+    counter
+  } = props;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Управляем числом: {counter}</h1>
+
+      <hr/>
+
+      <div className="Actions">
+        <button onClick={add}>Добавить 1</button>
+        <button onClick={() => {addNum(10)}}>Добавить 10</button>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './containers/appContainer';
+import {register} from './serviceWorker';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import rootReducer from './rootReducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// простой способ включения devtools
+// необходимо установить расширение браузера: https://github.com/zalmoxisus/redux-devtools-extension
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// оборачиваем App в Provider, передаём Store
+const app = (
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
+
+ReactDOM.render(app, document.getElementById('root'));
+register();
